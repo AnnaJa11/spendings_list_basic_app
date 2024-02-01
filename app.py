7513de9cdb2c1e94468c2b8d62e655c99a219bb2
An <151716513+AnnaJa11@users.noreply.github.com>
2024-02-01 08:18:53 +0000

    add comments with id in files app.py and forms.py

--- app.py
+++ app.py
@@ -69,28 +69,29 @@ def update_spending(spending_id):
 
 
 
-#------------------------
-# @app.route("/spendings/", methods = ["GET", "POST"])
-# def spendings_list():
-#     form = SpendingsForm()
-#     error = ""
-#     if request.method == "POST":
-#         if form.validate_on_submit():
-#             spendings.create(form.data)
-#             spendings.save_all()
-#         return redirect(url_for("spendings_list"))
-#     return render_template("spendings.html", form=form, spendings=spendings.all(), error=error)
+# ------------------------
+@app.route("/spendings/", methods = ["GET", "POST"])
+def spendings_list():
+    form = SpendingsForm()
+    # id = spendings.all()[-1]['id'] + 1
+    error = ""
+    if request.method == "POST":
+        if form.validate_on_submit():
+            spendings.create(form.data)
+            spendings.save_all()
+        return redirect(url_for("spendings_list"))
+    return render_template("spendings.html", form=form, spendings=spendings.all(), error=error)
 
-# @app.route("/spendings/<int:spending_id>/", methods=["GET", "POST"])
-# def spending_details(spending_id):
-#     spending = spendings.get(spending_id -1)
-#     form = SpendingsForm(data=spending)
+@app.route("/spendings/<int:spending_id>/", methods=["GET", "POST"])
+def spending_details(spending_id):
+    spending = spendings.get(spending_id -1)
+    form = SpendingsForm(data=spending)
 
-#     if request.method == "POST":
-#         if form.validate_on_submit():
-#             spendings.update(spending_id-1, form.data)
-#         return redirect(url_for("spendings_list"))
-#     return render_template("spending.html", form=form, spending_id=spending_id)
+    if request.method == "POST":
+        if form.validate_on_submit():
+            spendings.update(spending_id-1, form.data)
+        return redirect(url_for("spendings_list"))
+    return render_template("spending.html", form=form, spending_id=spending_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, IntegerField
 from wtforms.validators import DataRequired
 
 class SpendingsForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
     amount = FloatField('amount', validators=[DataRequired()])
+    # id = IntegerField('id', validators=[DataRequired()])
